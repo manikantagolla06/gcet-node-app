48e61426c17f0b53b6afdd3f7fcc50929623b706
ManikantaGolla06 <manikantakirangolla@gmail.com>
2025-06-05 11:01:39 +0530

    u

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-import express from 'express'
-import cors from 'cors'
-
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from 'dotenv';
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
 
-const app = express()
-app.listen(8080, () =>{
-    console.log("Server Started");
-});
-app.use(cors())
+dotenv.config();
+const app = express();
+app.use(cors());
 app.use(express.json());
+
+const MONGO_URI = process.env.MONGO_URI
+
 app.use("/users", userRouter);
-
 app.use("/products", productRouter);
-app.get("/", (req,res) => {
-    return res.send("Mod bits yesee");
-});
-app.get("/greet",(req,res)=>(res.send("Greetings")))
-app.get("/name",(req,res)=>(res.send("Manikanta kiran golla")))
-app.get("/Weather",(req,res)=>(res.send("69 degree")))
-app.get("/Movie",(req,res)=>(res.send("Khaleja")))
-.
+app.use("/orders",orderRouter)
 
-app.get("/products", (req, res) => {
-  const products = [
-    { name: "Product 1", price: 69 },
-    { name: "Product 2", price: 690 },
-    { name: "Product 3", price: 609 },
-  ];
-  res.json(products);
-});
-
-const userSchema = mongoose.Scheme({
-  name:{type:String},
-});
-
-const user = mongoose.model("User",userSchema);
-
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
